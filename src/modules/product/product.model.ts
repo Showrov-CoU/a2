ece22a77f867510f8d031350426897ab6afe23c0
Showrov-CoU <showrov.cse.cou@gmail.com>
2024-07-30 17:25:21 +0600

    add product interface and model

---
+++ src/modules/product/product.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+import { IInventory, IProduct, IVariant } from './product.interface';
+
+const VariantSchema = new Schema<IVariant>({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+});
+
+const InventorySchema = new Schema<IInventory>({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+const ProductSchema = new Schema<IProduct>(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    tags: { type: [String], required: true },
+    variant: { type: [VariantSchema], required: true },
+    inventory: { type: [InventorySchema], required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ProductModel = model<IProduct>('Product', ProductSchema);
